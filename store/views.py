912b9fbd0c7fb70088a52e59ade9f78b4963f5cd
MagZh0n <Magzhan.akhmet.07@gmail.com>
2025-04-08 20:49:26 +0500

    commit

--- store/views.py
+++ store/views.py
@@ -3,11 +3,6 @@ from .models import Product
 from rest_framework import viewsets
 from .serializers import ProductSerializer
 
-<<<<<<< HEAD
-
-=======
-# Веб-бет функциялары
->>>>>>> 94493687e42e90773dd67b5e2f82416f3a921731
 def home(request):
     return render(request, 'store/home.html')
 
@@ -22,11 +17,6 @@ def product_detail(request, product_id):
 def cart(request):
     return render(request, 'store/cart.html')
 
-<<<<<<< HEAD
-
-=======
-# DRF API viewset
->>>>>>> 94493687e42e90773dd67b5e2f82416f3a921731
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
