8d5f8d7910a12b75e7a02703ef9d53d361302d3d
an411k <zharkimbek06@mail.ru>
2025-04-22 19:59:55 +0500

    поправка

--- store/models.py
+++ store/models.py
@@ -1,12 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+from django.db import models
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    stock = models.PositiveIntegerField(default=0)  
+    stock = models.IntegerField()
     description = models.TextField()
-
+    image = models.ImageField(upload_to='products/', null=True, blank=True)  
+    
     def __str__(self):
         return self.name
 
