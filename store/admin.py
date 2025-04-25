0ec4746f76f25cd41b408145a33e8ed61a94a25f
an411k <zharkimbek06@mail.ru>
2025-04-25 10:06:36 +0500

    поправка

--- store/admin.py
+++ store/admin.py
@@ -1,13 +1,10 @@
 from django.contrib import admin
 from .models import Product, ProductImage
-<<<<<<< HEAD
 from django.contrib import admin
 from .models import Product, Order, OrderItem 
 
 admin.site.register(Order)
 admin.site.register(OrderItem)
-=======
->>>>>>> 8d5f8d7910a12b75e7a02703ef9d53d361302d3d
 
 class ProductImageInline(admin.TabularInline):  
     model = ProductImage
