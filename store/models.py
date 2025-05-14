09662540c3855ed25081614748794e09c2f64ac7
an411k <zharkimbek06@mail.ru>
2025-05-14 21:28:37 +0500

    aa

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
 
 class Product(models.Model):
@@ -7,8 +8,9 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     description = models.TextField()
-    image = models.ImageField(upload_to='products/', null=True, blank=True)  
-    
+    image = models.ImageField(upload_to='products/', null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)  
+
     def __str__(self):
         return self.name
 
